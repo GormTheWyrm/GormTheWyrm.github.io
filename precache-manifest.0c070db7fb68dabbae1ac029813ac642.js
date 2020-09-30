a108407463087a8035054210921318d9608cdfb8
Gorm <gdsoucie@gmail.com>
2020-09-30 13:24:24 -0400

    Updates

---
+++ precache-manifest.0c070db7fb68dabbae1ac029813ac642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a913a72906e4a66924ef1039771ecd7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1f0dc665b55b73def0",
+    "url": "/static/css/main.29014118.chunk.css"
+  },
+  {
+    "revision": "a8afc08aa5ac6c1bdd1d",
+    "url": "/static/js/2.2c33e8b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c33e8b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1f0dc665b55b73def0",
+    "url": "/static/js/main.79248cbe.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
